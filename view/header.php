d26d3502ab8f8eac24a5219bfdfcd8dc3c27e96e
cWenyu <cWenyu@users.noreply.github.com>
2019-02-04 16:37:05 +0000

    add new folder and create new files

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>The Bank System</title>
+    <link href="../main.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Bank Account</h1></header>
+       
