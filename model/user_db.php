3a2af59a8948ee8d36497b5d4c401d71e1afa522
cWenyu <cWenyu@users.noreply.github.com>
2019-02-14 21:55:09 +0000

    add new_register_user, delete_register, register_number functions

--- model/user_db.php
+++ model/user_db.php
@@ -1,28 +1,47 @@
 <?php
 
-function user_login($registerNumber, $registerPassword) {
+function new_register_user($registerPassword, $cardNumber, $cardHolder, $balance) {
     global $db;
-    $query = 'SELECT register_number,card_holder as users,balance
-              FROM users_account
-              where register_number =:registerNumber
-              and register_password =:registerPassword';
+    $query = 'INSERT INTO users_accounts
+                 (register_password,card_number,card_holder,balance)
+              VALUES
+                 (:registerPassword,:cardNumber,:cardHolder,:balance)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':registerPassword', $registerPassword);
+    $statement->bindValue(':cardHolder', $cardHolder);
+    $statement->bindValue(':cardNumber', $cardNumber);
+    $statement->bindValue(':balance', $balance);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function delete_register($registerNumber, $registerPassword) {
+    global $db;
+    $query = 'DELETE FROM users_accounts
+              WHERE register_number = :registerNumber
+              and register_password = :registerPassword';
     $statement = $db->prepare($query);
     $statement->bindValue(':registerNumber', $registerNumber);
     $statement->bindValue(':registerPassword', $registerPassword);
     $statement->execute();
+    $statement->closeCursor();
 }
 
-function new_register_user($cardNumber, $cardHolder, $balance, $registerPassword) {
+function register_number($registerPassword, $cardNumber, $cardHolder, $balance) {
     global $db;
-    $query = 'INSERT INTO users_account
-                 (card_number, card_holder, balance,register_password)
-              VALUES
-                 (:cardNumber, :cardHolder, :balance,:registerPassword)';
+    $query = 'SELECT register_number FROM users_accounts
+                 WHERE register_password = :registerPassword
+                 AND card_number = :cardNumber
+                 AND card_holder = :cardHolder
+                 AND balance = :balance';
     $statement = $db->prepare($query);
-    $statement->bindValue(':cardNumber', $cardNumber);
-    $statement->bindValue(':cardHolder', $cardHolder);
-    $statement->bindValue(':balance', $balance);    
     $statement->bindValue(':registerPassword', $registerPassword);
+    $statement->bindValue(':cardHolder', $cardHolder);
+    $statement->bindValue(':cardNumber', $cardNumber);
+    $statement->bindValue(':balance', $balance);
+    $registerNumber = $statement->execute();
+    $statement->closeCursor();
+    return $registerNumber;
 }
 
 ?>
