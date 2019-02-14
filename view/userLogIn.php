54cd5350461a29bb3d752a50d7fa0cedc6a1f7c5
cWenyu <cWenyu@users.noreply.github.com>
2019-02-14 21:50:19 +0000

    finish user login page

--- view/userLogIn.php
+++ view/userLogIn.php
@@ -1,28 +1,44 @@
-<?php include '../view/header.php'; ?>
 <?php
 session_start();
-
 if (isset($_SESSION['user_session']) != "") {
-    header("Location: home.php");
+    header("Location: ./view/user_home.php");
 }
 ?>
+<?php include '../include/header.php'; ?>
 <main>
-    <h2>User Login</h2>
-    <form action="home.php" method="post" id="login-form">
-        <label>Register Number:</label>
-        <input type="input" placeholder="Register Number" name="register_number" id="register_number"  >
-        <br>
-        <label>Password: </label>
-        <input type="password" placeholder="Register Password" name="password" id="register_password" />
-        <br>
-        <label>&nbsp;</label>
-        <button type="submit" class="btn btn-default" name="btn-login" id="btn-login">
-            <i aria-hidden="true"></i> &nbsp; Sign In
-        </button> 
-        <br> 
+    <h2>User Login</h2> 
+
+    <form method="post" id="login-form">
+
+        <div id="error">
+            <!-- error will be shown here ! -->
+        </div>
+
+        <div>
+            <input placeholder="Register Number" name="register_number" id="register_number" />
+            <span id="check-e"></span>
+        </div>
+
+        <div>
+            <input type="password" placeholder="Register Password" name="password" id="register_password" />
+        </div>
+
+        <hr />
+
+        <div>
+            <button type="submit" name="btn-login" id="btn-login">
+                <i aria-hidden="true"></i> &nbsp;
+                    Sign In
+            </button> 
+        </div>
+
     </form>
+    <p>
+        <a href="../index.php?action=list_products">List Products</a>
+    </p>
 </main>
-<?php include '../view/footer.php'; ?>
+<?php include '../include/footer.php'; ?>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://use.fontawesome.com/ee309940e2.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
