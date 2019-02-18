181146b470b643a88945ee1ecdfdc6c1e5fa3fe6
cWenyu <cWenyu@users.noreply.github.com>
2019-02-18 15:38:01 +0000

    fix problem of can not get card_holder and register number for new register

--- model/user_db.php
+++ model/user_db.php
@@ -39,9 +39,64 @@ function register_number($registerPassword, $cardNumber, $cardHolder, $balance) 
     $statement->bindValue(':cardHolder', $cardHolder);
     $statement->bindValue(':cardNumber', $cardNumber);
     $statement->bindValue(':balance', $balance);
-    $registerNumber = $statement->execute();
+    $statement->execute();
+    $registerNumber = $statement->fetch()[0];
     $statement->closeCursor();
     return $registerNumber;
+}
+
+function get_card_holder($registerNumber, $registerPassword) {
+    global $db;
+    $query = 'SELECT card_holder FROM users_accounts
+              WHERE register_number = :registerNumber
+              AND register_password = :registerPassword';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':registerNumber', $registerNumber);
+    $statement->bindValue(':registerPassword', $registerPassword);
+    $statement->execute();
+    $userNameA = $statement->fetch()[0];
+    $statement->closeCursor();
+    return $userNameA;
+}
+
+function buy_product($registerNumber, $cardHolder, $productCode, $productName, $quantity) {
+    global $db;
+    $query = 'INSERT INTO users_production
+                 (register_number,card_holder,product_code,product_name,quantity)
+              VALUES
+                 (:registerNumber,:cardHolder,:productCode,:productName,:quantity)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':registerNumber', $registerNumber);
+    $statement->bindValue(':cardHolder', $cardHolder);
+    $statement->bindValue(':productCode', $productCode);
+    $statement->bindValue(':productName', $productName);
+    $statement->bindValue(':quantity', $quantity);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function buy_product_change_balance($registerNumber, $cost) {
+    global $db;
+    $query = 'UPDATE users_accounts
+              SET balance = (balance - :cost)
+               WHERE register_number = :registerNumber';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':registerNumber', $registerNumber);
+    $statement->bindValue(':cost', $cost);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function user_production($registerNumber) {
+    global $db;
+    $query = 'SELECT * FROM users_production
+              WHERE register_number = :registerNumber';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':registerNumber', $registerNumber);
+    $statement->execute();
+    $userProducrs = $statement->fetchAll();
+    $statement->closeCursor();
+    return $userProducrs;
 }
 
 ?>
